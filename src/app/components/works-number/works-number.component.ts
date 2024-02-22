bf085d77e6f853ffc1958b23edcf2324f263dd58
Sim <64483242+SimBienvenueHoulBoumi@users.noreply.github.com>
2024-02-22 18:44:04 +0100

    add numbers

---
+++ src/app/components/works-number/works-number.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-works-number',
+  standalone: true,
+  imports: [],
+  templateUrl: './works-number.component.html',
+  styleUrl: './works-number.component.css'
+})
+export class WorksNumberComponent {
+
+}
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -3,6 +3,7 @@ import { HeaderComponent } from '../components/header/header.component';
 import { FooterComponent } from '../components/footer/footer.component';
 import { ProfileDescriptionComponent } from '../components/profile-description/profile-description.component';
 import { DevServicesComponent } from '../components/dev-services/dev-services.component';
+import { WorksNumberComponent } from '../components/works-number/works-number.component';
 
 @Component({
   selector: 'app-home',
@@ -12,6 +13,7 @@ import { DevServicesComponent } from '../components/dev-services/dev-services.co
     DevServicesComponent,
     FooterComponent,
     ProfileDescriptionComponent,
+    WorksNumberComponent
   ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css',
