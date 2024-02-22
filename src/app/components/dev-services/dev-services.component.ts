fd29338c1fd4115a46526d4c4f33554a1b531db0
Sim <64483242+SimBienvenueHoulBoumi@users.noreply.github.com>
2024-02-22 18:24:09 +0100

    add services

---
+++ src/app/components/dev-services/dev-services.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-dev-services',
+  standalone: true,
+  imports: [],
+  templateUrl: './dev-services.component.html',
+  styleUrl: './dev-services.component.css'
+})
+export class DevServicesComponent {
+
+}
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -2,11 +2,17 @@ import { Component } from '@angular/core';
 import { HeaderComponent } from '../components/header/header.component';
 import { FooterComponent } from '../components/footer/footer.component';
 import { ProfileDescriptionComponent } from '../components/profile-description/profile-description.component';
+import { DevServicesComponent } from '../components/dev-services/dev-services.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [HeaderComponent, FooterComponent, ProfileDescriptionComponent],
+  imports: [
+    HeaderComponent,
+    DevServicesComponent,
+    FooterComponent,
+    ProfileDescriptionComponent,
+  ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css',
 })
